49e511944489afbef48c114e3df80ca4805f1523
fabienv4 <fabienv4@hotmail.fr>
2023-04-04 10:48:28 +0200

    React Basics 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import PokemonCard from "./components/PokemonCard"
+import "./App.css";
+
 
 function App() {
-  return (
-    <div>
-      <PokemonCard />
-    </div>
-  )
-}
 
+  return (
+    <body>
+      <div>
+        <PokemonCard />
+      </div>
+    </body>
+  )
+
+}
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,35 @@
+import "../App.css";
+
+const pokemonList = [
+    {
+        name: "BULBIZARRE",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "NEW",
+    },
+];
+
 function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizarre" />
-            <figcaption>bulbasaur</figcaption>
-        </figure>
-    )
+
+    const cards = []
+    for (let i = 0; i < pokemonList.length; i++) {
+        const pokemon = pokemonList[i]
+        cards.push(
+            <figure className="card">
+                {pokemon.imgSrc === undefined ? (
+                    <p className="noCardImg">???</p>
+                ) : (
+                    <img className="cardImg" src={pokemon.imgSrc} alt={pokemon.name} />)}
+                <figcaption className="cardText">{pokemon.name}</figcaption>
+            </figure>
+        )
+    }
+    return cards
 }
+
+
+
 
 export default PokemonCard;
