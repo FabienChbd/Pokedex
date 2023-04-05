d9e5afda0ec3c35d57b18ef1fb5bee7dfc783e42
fabienv4 <fabienv4@hotmail.fr>
2023-04-05 13:35:36 +0200

    React Basics 06

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 import PokemonCard from "./components/PokemonCard"
 import "./App.css"
 import PropTypes from "prop-types"
+import { useState } from "react"
 
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0)
+  const currentPokemon = pokemonList[pokemonIndex]
+
+  const handleClickPrevious = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1)
+    }
+  }
+
+  const handleClickNext = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1)
+    }
+  }
 
   return (
     <body>
-      <div>
-        <PokemonCard pokemon={{ name: "BULBIZARRE", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" }} />
+      <h1>POKEDEX</h1>
+      <div className="pokedex">
+        <div>
+          <PokemonCard pokemon={currentPokemon} />
+        </div>
+        < div className="navigation">
+          {pokemonIndex > 0 && < button onClick={handleClickPrevious}>Précédent</button>}
+          {pokemonIndex < pokemonList.length - 1 && <button onClick={handleClickNext}>Suivant</button>}
+        </div>
       </div>
-    </body>
+    </body >
   )
-
 }
 
 const pokemonList = [
@@ -22,7 +43,22 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "NEW",
+    name: "SALAMECHE",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "CARAPUCE",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "PIKACHU",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "MEW",
   },
 ];
 
